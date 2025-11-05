e08bd40c9f319615179d24f564179d0db0f4d315
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2025-11-05 08:18:40 +0530

    backend client url added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,11 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL,
+	}),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
